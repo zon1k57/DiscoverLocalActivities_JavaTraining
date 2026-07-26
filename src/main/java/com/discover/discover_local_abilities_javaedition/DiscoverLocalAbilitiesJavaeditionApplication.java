c45a577300d7ebcbfd4a98f65ad079dd12204637
Viktor <viktorstojanovski32@gmail.com>
2026-07-26 20:37:04 +0200

    First commit

---
+++ src/main/java/com/discover/discover_local_abilities_javaedition/DiscoverLocalAbilitiesJavaeditionApplication.java
@@ -0,0 +1,13 @@
+package com.discover.discover_local_abilities_javaedition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverLocalAbilitiesJavaeditionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoverLocalAbilitiesJavaeditionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/discover/discover_local_abilities_javaedition/DiscoverLocalAbilitiesJavaeditionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discover.discover_local_abilities_javaedition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverLocalAbilitiesJavaeditionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
